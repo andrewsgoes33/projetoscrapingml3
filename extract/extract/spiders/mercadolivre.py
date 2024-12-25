688920fdcd4e6b1f3c30818ef8e20918425e66e8
Andrews Goes <andrews.goes.33@gmail.com>
2024-12-24 23:49:47 -0300

    feat: extract de 10 pag do ml completo

--- extract/extract/spiders/mercadolivre.py
+++ extract/extract/spiders/mercadolivre.py
@@ -5,17 +5,33 @@ class MercadolivreSpider(scrapy.Spider):
     name = "mercadolivre"
     allowed_domains = ["lista.mercadolivre.com.br"]
     start_urls = ["https://lista.mercadolivre.com.br/chinelo-masculino"]
+    page_count = 1
+    max_pages = 10
 
     def parse(self, response):
-        chinelos = response.css('div.poly-card__content')
+        slippers = response.css('div.poly-card__content')
         
-        for chinelo in chinelos:
+        
+        for slipper in slippers:
             
-            yield {'marca' : chinelo.css('span.poly-component__brand::text').get(),
-                   'nome' : chinelo.css('h2.poly-box.poly-component__title a::text').get()
-                   
-                   
-                   
+            prices = slipper.css('span.andes-money-amount__fraction::text').getall()
+            cents = slipper.css('span.andes-money-amount__cents::text').getall()
             
+            yield {'Brand' : slipper.css('span.poly-component__brand::text').get(),
+                   'Name' : slipper.css('h2.poly-box.poly-component__title a::text').get(),
+                   'Old_Price' : prices[0] if len(prices) > 0 else None,
+                   'Old_Cents' : cents[0] if len(cents) > 0 else None,
+                   'New_Price' : prices[1] if len(prices) > 1 else None,
+                   'New_Cents' : cents[1] if len(cents) > 1 else None,
+                   'Reviews_Rating' : slipper.css('span.poly-reviews__rating::text').get(),
+                   'Reviews_Total' : slipper.css('span.poly-reviews__total::text').get(),
+                   'Link' : slipper.css('h2.poly-box.poly-component__title a::attr(href)').get()
+                
             
             }
+            
+        if self.page_count < self.max_pages:    
+            next_page = response.css('li.andes-pagination__button.andes-pagination__button--next a::attr(href)').get()
+            if next_page:
+                self.page_count += 1
+                yield scrapy.Request(url=next_page, callback=self.parse)
